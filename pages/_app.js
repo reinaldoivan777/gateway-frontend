d4332ba5d332998523255ef1c3e249795893d8b7
Ivan <reinaldoivan777@gmail.com>
2020-01-31 02:17:19 +0700

    fix fontawesome build issue

--- pages/_app.js
+++ pages/_app.js
@@ -44,21 +44,6 @@ class MyApp extends App {
     const { Component, pageProps, store } = this.props;
     return (
       <Fragment>
-        <Head>
-          <link
-            rel='stylesheet'
-            href='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
-            integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T'
-            crossOrigin='anonymous'
-          />
-          <link href='https://fonts.googleapis.com/css?family=Nunito&display=swap' rel='stylesheet' />
-          <link
-            href='https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
-            rel='stylesheet'
-            integrity='sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN'
-            crossOrigin='anonymous'
-          />
-        </Head>
         <Provider store={store}>
           <PersistGate persistor={store.__PERSISTOR} loading={null}>
             <Wrapper>
--- pages/_document.js
+++ pages/_document.js
@@ -20,7 +20,23 @@ export default class MyDocument extends Document {
   render() {
     return (
       <html>
-        <Head>{this.props.styleTags}</Head>
+        <Head>
+          {this.props.styleTags}
+          <link
+            rel='stylesheet'
+            href='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
+            integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T'
+            crossOrigin='anonymous'
+          />
+          <link href='https://fonts.googleapis.com/css?family=Nunito&display=swap' rel='stylesheet' />
+          {/* <link
+            href='https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
+            rel='stylesheet'
+            integrity='sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN'
+            crossOrigin='anonymous'
+          /> */}
+          <link href='https://use.fontawesome.com/releases/v5.2.0/css/svg-with-js.css' rel='stylesheet' />
+        </Head>
         <body>
           <Main />
           <NextScript />
