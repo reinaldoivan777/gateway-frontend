6ec66a5086e8d646f5597a0d9e4c02fc054d132b
Ivan <reinaldoivan777@gmail.com>
2020-02-10 23:51:44 +0700

    update server.js

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const next = require('next');
-const routes = require('./routes');
-const app = next({ dev: process.env.NODE_ENV !== 'production' });
-const handler = routes.getRequestHandler(app, ({ req, res, route, query }) => {
-  app.render(req, res, route.page, query);
+const express = require('express');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
 });
 
-const { createServer } = require('http');
-app.prepare().then(() => {
-  createServer(handler).listen(3000);
-});
+// const routes = require('./routes');
+// const app = next({ dev: process.env.NODE_ENV !== 'production' });
+// const handler = routes.getRequestHandler(app, ({ req, res, route, query }) => {
+//   app.render(req, res, route.page, query);
+// });
+
+// const { createServer } = require('http');
+// app.prepare().then(() => {
+//   createServer(handler).listen(3000);
+// });
